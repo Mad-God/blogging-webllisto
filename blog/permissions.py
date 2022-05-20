4d07a6d84879762fa44ffe9e9f886a3eb24c1971
Satyam <stmsng2001@gmail.com>
2022-05-20 17:20:51 +0530

    Added Class Based View for the Update View of a blog. Added a new decorator to the blog_update() view to check the author. The CBV has 2 Mixins defined to check the author.

---
+++ blog/permissions.py
@@ -0,0 +1,59 @@
+from rest_framework import permissions
+from django.http import Http404
+from django.shortcuts import redirect, HttpResponse
+from django.contrib.auth.models import Permission
+from django.contrib.contenttypes.models import ContentType
+from .models import Blog
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+# content_type = ContentType.objects.get_for_model(Blog)
+# AuthorPermission = Permission.objects.create(
+#     codename='can_edit_blog',
+#     name='Can Edit Blog',
+#     content_type=content_type,
+# )
+
+
+
+def is_author_decorator(detail_view):
+    def wrapper_func(request, slug, *args, **kwargs):
+        blog = Blog.objects.get(slug = slug)
+        if request.user and request.user == blog.author:
+            return detail_view(request,slug,  *args, **kwargs)
+        else:
+            return HttpResponse("You are not the auhtor.")
+    return wrapper_func
+
+  
+class IsBlogAuthor(permissions.BasePermission):
+    
+    def has_object_permission(self, request, view, obj):            
+        breakpoint()
+        return True
+
+    def has_permission(self, request, view, obj):            
+        breakpoint()
+        return True
+    
+    
+
+class SameUserOnlyMixin(object):
+
+    def has_permissions(self):
+        # Assumes that your Article model has a foreign key called `auteur`.
+        return self.get_object().author == self.request.user
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.has_permissions():
+            raise Http404('You do not have permission. huehuehue')
+        return super(SameUserOnlyMixin, self).dispatch(
+            request, *args, **kwargs)
+
+
+class CheckAuthorMixin(LoginRequiredMixin, UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request == self.get_object().author
+
+    def handle_no_permission(self):
+        return HttpResponse("You are not the author. I am CheckAuthorMixin")
